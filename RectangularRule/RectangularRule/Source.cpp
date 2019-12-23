e860c6703901fcdf33a3968da416ad84b3c9d318
noobdev47 <therealguy79@yahoo.com>
2019-12-23 21:18:35 +0500

    Now with user input

--- RectangularRule/RectangularRule/Source.cpp
+++ RectangularRule/RectangularRule/Source.cpp
@@ -1,6 +1,20 @@
+//Group Number: 12
+//Group Members:
+//Muhammad Sumair Saif 3275-FBAS/BSSE/F16
+//Faheem Safdar        3269-FBAS/BSSE/F16
+//Section: A
+//Topic: Rectangular Rule
+//Course: Numerical Computing
+//Instructor: Sir Muhammad Rehan
+//Date: 23/12/2019
+
+//---------------------------------------------------
+
 //Libraries used for assistance in this program
 #include<iostream>
 #include<iomanip>
+#include<cmath>
+#define SIZE 100
 
 using namespace std;
 
@@ -8,28 +22,30 @@ using namespace std;
 double numericalCalculator(int, int, int, double, double, double[], double[]);
 double exactCalculator(int, int, int);
 double errorCalculator(double, double);
-void displayFunc(double[], double[], double, double, double);
+void displayFunc(double[], double[], double, double, double, int);
 
 int main()
 {
-	int n = 5;     //Number of Iterations
-	int a = 0;     //Lower Limit
-	int b = 1;     //Upper Limit
-	int power = 5; //Power of variable
+	int n = 0;        //Number of Iterations
+	int a = 0;        //Lower Limit
+	int b = 0;        //Upper Limit
+	int power = 0;    //Power of variable
 	double h = 0;
-
-	double valsOfx[5]; //Array for storing values of x0, x1, x2, x3, x4
-	double valsOfy[5]; //Array for storing values of y0, y1, y2, y3, y4
 
 	double numericalAnswer;
 	double exactAnswer;
 	double error;
 	double yValuesAdded = 0.0;
 
-	cout << "The Equation to be Numerically Integrated : x^5" << endl;
-	cout << "Lower Limit: " << a << endl;
-	cout << "Upper Limit: " << b << endl;
-	cout << "Number of Iterations: " << n << endl << endl;
+	//Taking Input for Various Parameters
+	cout << "The Expression : x" << endl;
+	cout << "Enter Lower Limit: "; cin >> a;
+	cout << "Enter Upper Limit: "; cin >> b;
+	cout << "Enter Power: "; cin >> power;
+	cout << "Enter Number of Iterations: "; cin >> n; cout << endl;
+
+	double valsOfx[SIZE]; //Array for storing values of x0, x1, x2 upto xn
+	double valsOfy[SIZE]; //Array for storing values of y0, y1, y2 upto yn
 
 	numericalAnswer = numericalCalculator(a, b, n, h, yValuesAdded, valsOfx, valsOfy);
 
@@ -37,13 +53,13 @@ int main()
 
 	error = errorCalculator(numericalAnswer, exactAnswer);
 
-	displayFunc(valsOfx, valsOfy, numericalAnswer, exactAnswer, error);
+	displayFunc(valsOfx, valsOfy, numericalAnswer, exactAnswer, error, n);
 }
 
 //This function calculates numerical answer for given expression
 double numericalCalculator(int a, int b, int n, double h, double yValuesTotal, double valsOfx[], double valsOfy[])
 {
-	h = (b - a) / 5.0;
+	h = (b - a) / static_cast<float>(n);
 
 	for (int i = 0; i <= n - 1; i++)
 	{
@@ -61,7 +77,7 @@ double exactCalculator(int a, int b, int power)
 {
 	power += 1;
 
-	return ((b ^ power) / 6.0) - ((a ^ power) / 6.0);
+	return (pow(b, power) / (float)power) - (pow(a, power) / (float)power);
 }
 
 //This function calculates difference between numerical and exact answers for given expression
@@ -71,17 +87,16 @@ double errorCalculator(double numerical, double exact)
 }
 
 //This function's sole purpose is to display various values and artifacts 
-void displayFunc(double valsOfx[], double valsOfy[], double numericalAnswer, double exactAnswer, double error)
+void displayFunc(double valsOfx[], double valsOfy[], double numericalAnswer, double exactAnswer, double error, int n)
 {
-	cout << "x0 = " << setprecision(4) << valsOfx[0] << "      " << "y0 = " << setprecision(4) << valsOfy[0] << endl;
-	cout << "x1 = " << setprecision(4) << valsOfx[1] << "    " << "y1 = " << setprecision(4) << valsOfy[1] << endl;
-	cout << "x2 = " << setprecision(4) << valsOfx[2] << "    " << "y2 = " << setprecision(4) << valsOfy[2] << endl;
-	cout << "x3 = " << setprecision(4) << valsOfx[3] << "    " << "y3 = " << setprecision(4) << valsOfy[3] << endl;
-	cout << "x4 = " << setprecision(4) << valsOfx[4] << "    " << "y4 = " << setprecision(4) << valsOfy[4] << endl;
+	for (int i = 0; i <= n - 1; i++)
+	{
+		cout << "x" << i << " = "<< setprecision(6) << valsOfx[i] << "      " << "y" << i << " = " << setprecision(6) << valsOfy[i] << endl;
+	}
 
 	cout << endl;
 
-	cout << "Numerical Answer = " << setprecision(4) << numericalAnswer << endl;
+	cout << "Numerical Answer = " << setprecision(6) << numericalAnswer << endl;
 	cout << "Exact Answer = " << exactAnswer << endl;
 	cout << "Error = " << error << endl << endl;
 }
